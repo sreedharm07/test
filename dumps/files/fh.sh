6dddc0fc2a3b553d68ddde3e27ec562e8e2ea2ed
sreedhar <sreedhar.m@zinier.com>
2025-08-05 12:44:13 +0530

    sqs import

--- dumps/files/fh.sh
+++
@@ -1,5 +0,0 @@
-
-
-sum(container_memory_working_set_bytes{namespace="$namespace", pod=~"$pod",container="$app"}) by (pod) / sum(kube_pod_container_resource_requests{namespace="$namespace", pod=~"$pod",resource="memory",container="$app"}) by (pod)
-sum(container_memory_working_set_bytes{namespace="$namespace", pod=~"$pod",container="$app"}) by (pod) / sum(kube_pod_container_resource_limits{namespace="$namespace", pod=~"$pod", unit="byte", container="$app"}) by (pod)
-
